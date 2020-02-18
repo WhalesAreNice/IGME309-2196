262e7be0a8a5febbf32b3fe45ca402fb326c55e1
WhalesAreNice <an1994xp@gmail.com>
2020-02-18 00:12:38 -0500

    E07 - Finished

--- E07 - Gimbal Removal/AppClassControls.cpp
+++ E07 - Gimbal Removal/AppClassControls.cpp
@@ -416,24 +416,36 @@ void Application::ProcessKeyboard(void)
 #pragma endregion
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::X))
 	{
-		if (fMultiplier)
-			m_v3Rotation.x -= 1.0f;
-		else
-			m_v3Rotation.x += 1.0f;
+		//m_qOrientation.x++;
+		quaternion q1 = glm::angleAxis(0.1f, vector3(1.0f, 0.0f, 0.0f));
+		m_qOrientation = m_qOrientation * q1;
+
+		//if (fMultiplier)
+		//	//m_v3Rotation.x -= 1.0f;
+		//else
+		//	m_v3Rotation.x += 1.0f;
 	}
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Y))
 	{
-		if (fMultiplier)
-			m_v3Rotation.y -= 1.0f;
-		else
-			m_v3Rotation.y += 1.0f;
+		//m_qOrientation.y++;
+		quaternion q1 = glm::angleAxis(0.1f, vector3(0.0f, 1.0f, 0.0f));
+		m_qOrientation = m_qOrientation * q1;
+
+		//if (fMultiplier)
+		//	m_v3Rotation.y -= 1.0f;
+		//else
+		//	m_v3Rotation.y += 1.0f;
 	}
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Z))
 	{
-		if (fMultiplier)
-			m_v3Rotation.z -= 1.0f;
-		else
-			m_v3Rotation.z += 1.0f;
+		//m_qOrientation.z++;
+		quaternion q1 = glm::angleAxis(0.1f, vector3(0.0f, 0.0f, 1.0f));
+		m_qOrientation = m_qOrientation * q1;
+
+		//if (fMultiplier)
+		//	m_v3Rotation.z -= 1.0f;
+		//else
+		//	m_v3Rotation.z += 1.0f;
 	}
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::R))
 	{
